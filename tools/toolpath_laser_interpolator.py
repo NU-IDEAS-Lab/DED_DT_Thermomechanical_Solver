65b9e2eb730d7a3e2bb124486e5a936205045dc5
Rujing Zha <geek1599@gmail.com>
2023-10-30 21:23:41 -0500

    added interpolator

--- tools/toolpath_laser_interpolator.py
+++ tools/toolpath_laser_interpolator.py
@@ -12,22 +12,32 @@ def print_rel_movement(textfile, move_vec, scan_speed):
                   + " Z" + str(z_move)
                   + " F" + str(scan_speed))
         output_text_file.write(string + os.linesep)
+        
+# Mode
+laser_power_mode = "const"        
 
-# load laser power profile
-geom_name = "thin_wall"
-laser_profile = "NLP_56"
+geom_name = "circle_control"
+laser_profile = 600.;
 project = "PSED"
-laser = "GUIDE"
-output_toolpath_path = project + "_" + laser_profile + ".txt"
 
-f = open(output_toolpath_path, "w")
-f.close()
+# "GUIDE" or "MAIN"
+laser = "GUIDE"
 
 # Parameters
 interp_freq = 1.0 # in hz - approximate number of times to update per second.
 
-laserprof_loc = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "laser_inputs", geom_name, laser_profile)
-laser_hist = np.loadtxt(laserprof_loc + ".csv", skiprows=1, delimiter=',')
+if laser_power_mode == "profile":
+    laserprof_loc = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "laser_inputs", geom_name, laser_profile)
+    laser_hist = np.loadtxt(laserprof_loc + ".csv", skiprows=1, delimiter=',')
+    output_toolpath_path = project + "_" + laser_profile + "_" + laser + ".txt"
+elif laser_power_mode == "const":
+    output_toolpath_path = project + "_" + str(laser_profile) + "W_const_" + laser + ".txt"
+else:
+    raise Exception("Invalid laser profile type")
+
+f = open(output_toolpath_path, "w")
+f.close()
+
 # Load toolpath
 toolpath_loc = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "examples", project, "toolpath.crs")
 toolpath = np.loadtxt(toolpath_loc)
@@ -36,6 +46,10 @@ old_laser_state = False     # False for turned off
 
 current_time = toolpath[0, 0];
 current_location = toolpath[0, 1:4]
+if laser_power_mode == "const":
+    with open(output_toolpath_path, 'a') as output_obj:
+        output_obj.write("$AO[0].X = " + str(laser_profile / 100.) + os.linesep)
+        
 for itr in range(1, toolpath.shape[0]):
     # Old destination is previous point
     old_time = current_time
@@ -59,17 +73,17 @@ for itr in range(1, toolpath.shape[0]):
     if old_laser_state and new_laser_state:
         # Laser is on and stays on
         # Number of time steps
-        num_steps = np.int64(delT_move*interp_freq)
+        num_steps = min(np.int64(delT_move*interp_freq), 1)
         delT_laserchange = delT_move / num_steps
         move_vector_laserchange = move_vector / num_steps
         for jtr in range(0, num_steps):
             time_jtr = old_time + (jtr * delT_laserchange)
-            laser_power = np.interp(time_jtr, laser_hist[:, 1], laser_hist[:, 0])
-
-            print(time_jtr)
+            
             # Print laser power
-            with open(output_toolpath_path, 'a') as output_obj:
-                output_obj.write("$AO[0].X = " + str(laser_power / 100.) + os.linesep)
+            if laser_power_mode == "profile":
+                laser_power = np.interp(time_jtr, laser_hist[:, 1], laser_hist[:, 0])
+                with open(output_toolpath_path, 'a') as output_obj:
+                    output_obj.write("$AO[0].X = " + str(laser_power / 100.) + os.linesep)
             print_rel_movement(output_toolpath_path, move_vector_laserchange, scan_speed)
         old_laser_state = 1
 
@@ -85,6 +99,8 @@ for itr in range(1, toolpath.shape[0]):
                 output_obj.write("$DO[1].X = 0" + os.linesep)
             elif laser == "MAIN":
                 output_obj.write("$DO[0].X = 0" + os.linesep)
+            else:
+                raise Exception("illegal laser mode!")
         print_rel_movement(output_toolpath_path, move_vector, scan_speed)
         old_laser_state = 0
 
@@ -95,15 +111,18 @@ for itr in range(1, toolpath.shape[0]):
                 output_obj.write("$DO[1].X = 1" + os.linesep)
             elif laser == "MAIN":
                 output_obj.write("$DO[0].X = 1" + os.linesep)
-        num_steps = np.int64(delT_move*interp_freq)
+                
+            else:
+                raise Exception("illegal laser mode!")
+        num_steps = min(np.int64(delT_move*interp_freq), 1)
         delT_laserchange = delT_move / num_steps
         move_vector_laserchange = move_vector / num_steps
         for jtr in range(0, num_steps):
             time_jtr = old_time + (jtr * delT_laserchange)
-            laser_power = np.interp(time_jtr, laser_hist[:, 1], laser_hist[:, 0])
-            print(time_jtr)
             # Print laser power
-            with open(output_toolpath_path, 'a') as output_obj:
-                output_obj.write("$AO[0].X = " + str(laser_power / 100.) + os.linesep)
+            if laser_power_mode == "profile":
+                laser_power = np.interp(time_jtr, laser_hist[:, 1], laser_hist[:, 0])
+                with open(output_toolpath_path, 'a') as output_obj:
+                    output_obj.write("$AO[0].X = " + str(laser_power / 100.) + os.linesep)
             print_rel_movement(output_toolpath_path, move_vector_laserchange, scan_speed)
         old_laser_state = 1
